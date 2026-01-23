4772c7e24ed9638605bbc40e2e2351536228840f
JerinXplore <jerinjerin101325@gmail.com>
2026-01-24 03:26:00 +0600

    fix(auth): fixed login route path in auth router

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -5,7 +5,7 @@ import { AuthController } from "./auth.controller";
 const router = Router();
 
 router.post('/register',AuthController.register);
-router.post('login',AuthController.login);
+router.post('/login',AuthController.login);
 router.post('/refresh',AuthController.refreshToken);
 
 export default router;
