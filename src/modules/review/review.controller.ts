543b1b0aa3b409bed5e8d299cc97afa622b12883
JerinXplore <jerinjerin101325@gmail.com>
2026-01-25 02:04:18 +0600

    feat(review): implemented review controller

---
+++ src/modules/review/review.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { ReviewService } from "./review.service";
+
+
+const createReview= async (req:any, res:Response)=>{
+    try{
+        const {coffeeId,rating,comment}=req.body;
+        const userId = req.userId;
+
+        const result = await ReviewService.createReview(userId,coffeeId,rating,comment);
+
+        res.status(201).json({
+            success: true,
+            message: "Review added successfully",
+            data: result,
+        });
+    } catch (error: any){
+        res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+};
+
+const getCoffeeReviews= async(req: Request, res:Response)=>{
+    const {id} = req.params;
+
+    const result = await ReviewService.getCoffeeReviews(id as string);
+
+    res.status(200).json({
+    success: true,
+    data: result,
+  });
+};
+
+export const ReviewController={
+    createReview,
+    getCoffeeReviews,
+}
