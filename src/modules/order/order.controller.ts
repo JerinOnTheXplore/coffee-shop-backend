0c818823b951923a1f65d5d07ed7191ec70c517c
JerinXplore <jerinjerin101325@gmail.com>
2026-01-26 00:49:32 +0600

    feat(order): implemented order controller for create and fetch orders

---
+++ src/modules/order/order.controller.ts
@@ -0,0 +1,48 @@
+import { Response } from "express";
+import { OrderService } from "./order.service";
+
+
+const createOrder = async (req:any,res:Response)=>{
+    try{
+        const userId= req.user.userId;
+        const {items} = req.body;
+
+        const result = await OrderService.createOrder(userId,items);
+
+        res.status(201).json({
+            success: true,
+            message: "Order placed successfully..",
+            data:result,
+        });
+    } catch(error:any){
+        res.status(400).json({
+        success: false,
+        message: error.message,
+        });
+    }
+};
+
+const getMyOrders= async(req:any,res:Response)=>{
+    const userId= req.user.userId;
+    const result = await OrderService.getMyOrders(userId);
+
+    res.status(200).json({
+    success: true,
+    data: result,
+  });
+};
+
+const getAllOrders= async(req:Request,res:Response)=>{
+    const result= await OrderService.getAllOrders();
+
+    res.status(200).json({
+        success:true,
+        data:result,
+    });
+};
+
+export const orderController = {
+ createOrder,
+ getMyOrders,
+ getAllOrders,
+}
